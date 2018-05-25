55c2f8f801fe5a12927c8630cd2f1671ebc277d4
Piotr Kasperski <piotrkas@kainos.com>
2018-05-25 12:05:10 +0100

    Installing modules by librarian-puppet

--- entrypoints/develop.sh
+++ entrypoints/develop.sh
@@ -3,6 +3,10 @@
 # Load common settings 
 . /etc/init.d/common.sh
 
+cd ${BUILD_DIR}/puppet/main
+librarian-puppet install --local
+
+
 echo ${ROLE}
 FACTER_role=${ROLE} puppet apply --environment main /etc/puppetlabs/code/environments/main/manifests/site.pp
 
